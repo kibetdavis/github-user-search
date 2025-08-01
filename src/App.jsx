039e4a5e8f2d6a0b2f1c9a3f5d7eb66059523fb8
Isa8205 <kibetdavis848@gmmail.com>
2025-08-01 15:13:24 +0300

    Add GitHub User Search feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import SearchUser from './components/SearchUser';
+import Search from './components/Search';
 
-function App() {
+const App = () => {
   return (
     <div>
       <h1>GitHub User Search</h1>
-      <SearchUser />
+      <Search />
     </div>
   );
-}
+};
 
 export default App;
